b64e710fcf50894521f03530ea93223f0725ab26
Jimmy022363 <48532827+Jimmy022363@users.noreply.github.com>
2019-05-31 14:49:40 -0500

    Add files via upload

---
+++ src/components/BookForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    
+    <div>
+        <label for="finishedReading">
+            Finished Reading
+            <input type="checkbox" v-model="bookData.finishedReading">
+        </label>
+     </div>
+
+     <div>
+         <label for="borrowed">
+             Borrowed
+            <input type="radio" value="borrowed" v-model="bookData.ownership"> 
+         </label>     
+
+         <label for="bought">
+             Bought
+             <input type="radio" value="bought" v-model="bookData.ownership"> 
+         </label>
+     </div>
+
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+        bookData: {
+            bookTitle: "",
+            bookAuthor: "",
+            finishedReading: false,
+            ownership: []
+        }
+      
+    };
+  },
+  methods: {
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
